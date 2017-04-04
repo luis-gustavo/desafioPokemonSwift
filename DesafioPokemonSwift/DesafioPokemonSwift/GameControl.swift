100e45fd855f92e481fe996bd73bec2547991a74
Luis Gustavo <luis.gustavo3013@hotmail.com>
2017-04-04 18:17:49 -0300

    adicionado alguns metodos de gamecontrol

--- DesafioPokemonSwift/DesafioPokemonSwift/GameControl.swift
+++ DesafioPokemonSwift/DesafioPokemonSwift/GameControl.swift
@@ -10,4 +10,27 @@ import Foundation
 
 class GameControl{
     
+    
+    class func ordernarPokemonsJogador(jogador:Jogador){
+        var coisas : [Pokemon] = []
+        coisas = jogador.pokemons.sorted(by: { $0.level > $1.level})
+        jogador.pokemons = coisas
+    }
+    
+    class func evoluirPokemon(pokemon:Pokemon){
+        if pokemon.experiencia >= 100 {
+            pokemon.experiencia = pokemon.experiencia-100
+            pokemon.level += 1
+            Visao.pokemonEvoluiu(pokemon: pokemon)
+        }
+    }
+    
+    class func mostrarStatusJogador(currentJogador:Jogador){
+        Visao.limpaTela()
+        Visao.menuEstatus(jogador: currentJogador)
+    }
+    
+    class func escolhaLutar(jogador:Jogador, pokemon:Pokemon){
+        
+    }
 }
