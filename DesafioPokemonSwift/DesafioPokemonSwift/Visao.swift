694428e9a4fff8c53a885a46d1f2dfab4f3059e9
Geovanni Oliveira <geovannirock@gmail.com>
2017-04-04 17:47:51 -0300

    conversão de 4 métodos de objctive c para swift

--- DesafioPokemonSwift/DesafioPokemonSwift/Visao.swift
+++ DesafioPokemonSwift/DesafioPokemonSwift/Visao.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+class Visao{
+    
+    class func naoCapturouPokemon(){
+        //chamar funcao limpa tela
+        print("Nao foi dessa vez! Voce nao conseguiu capturar o pokemon\n")
+    }
+    
+    class func menuLutarJogador(jogador: Jogador){
+        
+        print("Escolha o pokemon para batalhar!\n")
+        let contador = 1
+        
+        for pokemon in jogador.pokemons {
+           print("Digite \(contador) para escolher o pokemon \(pokemon.nome) de nível \(pokemon.level) de elemento \(pokemon.tipo)")
+        }
+    }
+    
+    class func menuEstatus(jogador: Jogador){
+        var pokemonAtual = 1
+        
+        print("Nome: \(jogador.nome)")
+        print("Nº de pokemons: \(jogador.pokemons.count)")
+        
+        for _ in jogador.pokemons{
+            print("\(pokemonAtual) ||\(jogador.pokemons[pokemonAtual].nome)||")
+            print(" Tipo: \(jogador.pokemons[pokemonAtual].tipo)")
+            print(" Nível: \(jogador.pokemons[pokemonAtual].level)")
+            print(" Exp: \(jogador.pokemons[pokemonAtual].experiencia)")
+            
+            pokemonAtual+=1
+        }
+        
+    }
+    
+    class func fugiu(){
+        //colocar limpaTela
+        print("Você fugiu!")
+    }
+    
+}
+
