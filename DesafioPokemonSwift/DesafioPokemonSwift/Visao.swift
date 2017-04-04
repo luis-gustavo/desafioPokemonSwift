69377e5572a240f0e627f0384edac1daf2d3befd
Luis Gustavo <luis.gustavo3013@hotmail.com>
2017-04-04 16:38:23 -0300

    Criado documento de Visao

---
+++ DesafioPokemonSwift/DesafioPokemonSwift/Visao.swift
@@ -0,0 +1,9 @@
+//
+//  Visao.swift
+//  DesafioPokemonSwift
+//
+//  Created by Luis Gustavo Avelino de Lima Jacinto on 04/04/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import Foundation
